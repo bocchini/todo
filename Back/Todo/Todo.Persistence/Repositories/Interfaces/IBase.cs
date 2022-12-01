752bf65cf60dd299f9f2b1dea5024d4a1552c108
Americo Bocchini <albocchini@gmail.com>
2022-12-01 16:17:37 -0300

    Inicio persistence

---
+++ Back/Todo/Todo.Persistence/Repositories/Interfaces/IBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Persistence.Repositories.Interfaces;
+
+public interface IBase
+{
+    void Add<T>(T entity) where T : class;
+    void Update<T>(T entity) where T : class;
+    void Delete<T>(T entity) where T : class;
+    void DeleteRange<T>(T entity) where T : class;
+    Task<bool> SaveChangesAsync();
+}
