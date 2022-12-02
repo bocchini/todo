026cfa3ec0c71fa0acb3dade21d2034c5649e9be
Americo Bocchini <albocchini@gmail.com>
2022-12-02 12:10:02 -0300

    Criação Builder de Categorias e testes do CategoriasService

--- Back/Todo/Todo.Tests/Application/Services/CategoriaServiceTests.cs
+++ Back/Todo/Todo.Tests/Application/Services/CategoriaServiceTests.cs
@@ -1,6 +1,10 @@
-﻿using NSubstitute;
+﻿using Bogus;
+using FluentAssertions;
+using NSubstitute;
 using Todo.Application.Services;
+using Todo.Domain.Entities;
 using Todo.Persistence.Repositories.Interfaces;
+using Todo.Tests.Builders;
 
 namespace Todo.Tests.Application.Services;
 
@@ -8,16 +12,105 @@ public class CategoriaServiceTests
 {
     private readonly CategoriaService _service;
     private readonly ICategoriaRepository _repository;
+    private readonly CategoriaBuilder _categoriaBuilder;
+    private readonly Faker _faker;
 
     public CategoriaServiceTests()
     {
         _repository = Substitute.For<ICategoriaRepository>();
         _service = new CategoriaService(_repository);
+        _categoriaBuilder = new CategoriaBuilder();
+        _faker = new Faker();
     }
 
     [Fact]
-    public void Deve_Adicionar_Categoria()
+    public async Task Add_Deve_Adicionar_CategoriaAsync()
     {
+        var categoria = _categoriaBuilder.GeraCategoria();
+        _repository.Add(categoria);
 
+        _repository.SaveChangesAsync().Returns(true);
+        categoria.Id = _faker.Random.Int(0, 100);
+
+        _repository.GetUmaCategoriaAsync(Arg.Any<int>()).Returns(categoria);
+
+        var result = await _service.Add(categoria);
+
+        result.Should().BeEquivalentTo(categoria);
+    }
+
+    [Fact]
+    public async Task Add_Deve_Retornar_Null_ErroAoSalvar()
+    {
+        var categoria = new Categoria();
+        
+        _repository.Add(categoria);
+        _repository.SaveChangesAsync().Returns(false);
+
+        var result = await _service.Add(categoria);
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task Delete_Deve_Remover_UmaCategoria_ComSucesso()
+    {
+        var categoria = _categoriaBuilder.GeraCategoria(true);
+
+        _repository.GetUmaCategoriaAsync(Arg.Any<int>()).Returns(categoria);
+        _repository.Delete(categoria);
+        _repository.SaveChangesAsync().Returns(true);
+
+        var result = await _service.Delete(categoria);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Delete_Deve_DarErro_QuandoCategoriaNaoEEncontrada()
+    {
+        var categoria = new Categoria();
+
+        _repository.GetUmaCategoriaAsync(Arg.Any<int>()).Returns(Task.FromResult<Categoria>(null));
+
+        var result = await Assert.ThrowsAsync<Exception>(() => _service.Delete(categoria));
+     
+        result.Message.Should().Be("Categoria não encontrada");
+    }
+
+    [Fact]
+    public async Task Delete_Deve_RetornarFalse_QaundoNaoConseguirSalvar()
+    {
+        var categoria = new Categoria();
+
+        _repository.GetUmaCategoriaAsync(Arg.Any<int>()).Returns(categoria);
+        _repository.Delete(categoria);
+        _repository.SaveChangesAsync().Returns(false);
+
+        var result = await _service.Delete(categoria);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Desativar_Deve_Desativa_Categoria()
+    {
+        var categoria = _categoriaBuilder.GeraCategoria(true);
+
+        _repository.GetUmaCategoriaAsync(Arg.Any<int>()).Returns(categoria);
+        _repository.Update(categoria);
+        _repository.SaveChangesAsync().Returns(true);
+
+        var result = await _service.Desativar(categoria);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Desativar_Deve_RetornarErro_SeCategoriaNaoEncontrada()
+    {
+        var categoria = new Categoria();
+
+        _repository.GetUmaCategoriaAsync(Arg.Any<int>()).Returns(Task.FromResult<Categoria>(null));
+
+        var result = await Assert.ThrowsAsync<Exception>(() => _service.Desativar(categoria));
+
+        result.Message.Should().Be("Categoria não encontrada");
     }
 }
--- Back/Todo/Todo.Tests/Builders/CategoriaBuilder.cs
+++ Back/Todo/Todo.Tests/Builders/CategoriaBuilder.cs
@@ -1,5 +1,31 @@
-﻿namespace Todo.Tests.Builders;
+﻿using AutoBogus;
+using Todo.Domain.Entities;
 
-public class CategoriaBuilder
+namespace Todo.Tests.Builders;
+
+public class CategoriaBuilder : AutoFaker<Categoria>
 {
+    public Categoria GeraCategoria(bool comId = false)
+    {
+        return comId ? 
+            ComId().ComNome().ComEstaAtiva().Generate() :
+            ComNome().ComEstaAtiva().Generate();
+    }
+    public CategoriaBuilder ComId()
+    {
+        RuleFor(c => c.Id, ca => ca.Random.Int(0, 100));
+        return this;
+    }
+
+    public CategoriaBuilder ComNome()
+    {
+        RuleFor(c => c.Nome, ca => ca.Name.FirstName());
+        return this;
+    }
+
+    public CategoriaBuilder ComEstaAtiva()
+    {
+        RuleFor(c => c.Ativa, ca => ca.Random.Bool());
+        return this;
+    }
 }
